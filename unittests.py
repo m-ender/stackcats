e4a4a811dfaa73b46ab25318a3c5940934f6c139
Kevin <kevinliang_888@hotmail.com>
2015-10-29 03:25:47 +1100

    Unit tests

---
+++ unittests.py
@@ -0,0 +1,40 @@
+import io
+import sys
+import unittest
+
+from stackcats import StackCats
+
+class TestStackCats(unittest.TestCase):
+    def setUp(self):
+        sys.stdout = io.StringIO()
+
+    def test_HW(self):
+        self.run_test("<13>>33>100>108>114>111>87>32>44>111>108>108>101>72<<<<<<<<<<<<<r<;"
+                      ">r>>>>>>>>>>>>>27<101<801<801<111<44<23<78<111<411<801<001<33<<31>",
+                      "Hello, World!")
+
+    def run_test(self, prog, output):
+        if isinstance(prog, tuple):
+            prog, input_ = prog
+        else:
+            input_ = ""
+
+        sc = StackCats(prog)
+        sc.run(input_)
+        self.assertEqual(self.output(), output)
+
+        # Assert cat without middle
+        if len(prog) % 2 == 1:
+            prog = prog[:len(prog)//2] + prog[len(prog)//2 + 1:]
+
+            sc = StackCats(prog)
+            sc.run(input_)
+            self.assertEqual(self.output(), input_)
+
+    def output(self):
+        val = sys.stdout.getvalue()
+        sys.stdout = io.StringIO()
+        return val
+
+if __name__ == '__main__':
+    unittest.main()
