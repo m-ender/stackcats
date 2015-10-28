4b7cff7a7feb7e941390bffbc94462c115533148
Kevin <kevinliang_888@hotmail.com>
2015-10-29 05:07:42 +1100

    More tests

--- unittests.py
+++ unittests.py
@@ -16,17 +16,26 @@ class TestStackCats(unittest.TestCase):
         
         self.run_test((code, "abcde"), "a")
         self.run_test((code, "123456"), "1")
+        self.run_test((code, "ABCDEFGHI"), "A")
 
     def test_last_byte(self):
         code = "r1;1r"
         
         self.run_test((code, "abcde"), "e")
         self.run_test((code, "123456"), "6")
+        self.run_test((code, "ABCDEFGHI"), "I")
 
     def test_HW(self):
         self.run_test("<13>>33>100>108>114>111>87>32>44>111>108>108>101>72<<<<<<<<<<<<<r<;"
                       ">r>>>>>>>>>>>>>27<101<801<801<111<44<23<78<111<411<801<001<33<<31>",
                       "Hello, World!")
+
+    def test_drop_first_three(self):
+        code = "]]]#[>[[[<r<;>r>]]]<]#[[["
+
+        self.run_test((code, "abcde"), "de")
+        self.run_test((code, "123456"), "456")
+        self.run_test((code, "ABCDEFGHI"), "DEFGHI")
 
     def run_test(self, prog, output):
         if isinstance(prog, tuple):
